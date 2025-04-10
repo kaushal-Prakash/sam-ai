51994aab77296a1197e67890f59c9cc60f8ec955
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-04-10 23:10:34 +0530

    footer done

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer relative'>
+        <img src='/logo.png' alt='footer logo' className='footer__img'/>
+        <p className='footer__p1'> VARUN AI</p>
+        <p className='footer__p2'>&copy; VARUN AI. ALL RIGHTS RESERVED</p>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,18 +1,25 @@
-import Image from 'next/image'
-import React from 'react'
+import React from 'react';
 
 function Hero() {
   return (
-    <div className='min-h-screen w-full'>
-        <Image
-          src="/hero/trees-1.png"
-          alt="trees 1"
-          width={1000}
-          height={1000}
-          className="object-cover w-full h-[400px] md:h-[600px] lg:h-[800px] xl:h-[900px]"
-        />
+    <div className="relative w-full h-screen overflow-hidden hero">
+      <img
+        src="/hero/trees-1.png"
+        alt="trees 1"
+        className="absolute w-full h-full object-fill object-center tree-1"
+      />
+      <img
+        src="/hero/trees-2.png"
+        alt="trees 2"
+        className="absolute w-full h-full object-fill object-center tree-2"
+      />
+      <img
+        src="/hero/flame-2.png"
+        alt="flame 2"
+        className="absolute w-full h-full object-cover object-center flame-2"
+      />
     </div>
-  )
+  );
 }
 
 export default Hero
